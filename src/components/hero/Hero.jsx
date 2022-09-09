80c834bc1901af989b73820109b58c6caa395921
felpfsf <feferreira.sf@gmail.com>
2022-09-09 19:00:50 -0300

    update: hero component

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='h-screen bg-[url("/assets/banner-hero-mobile.png")] sm:bg-[url("/assets/banner-hero.png")] bg-no-repeat'>
+      Hero Content
+    </section>
+  )
+}
+
+export default Hero
