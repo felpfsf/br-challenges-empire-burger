7e435d00f972727298ee4529b9cf249c121a1a84
felpfsf <feferreira.sf@gmail.com>
2022-09-09 18:59:43 -0300

    update: project color palette

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,17 @@ module.exports = {
       fontFamily: {
         heading: ['Lilita One', 'cursive'],
         sans: ['Lato', 'sans-serif']
+      },
+      colors: {
+        burgerPalette: {
+          red: '#F43127',
+          yellow: '#F59A1B',
+          title_black_38: '#1D0605',
+          title_black_60: '#635352',
+          title_black_87: '#34201F',
+          beige: '#FAE2CB',
+          brown: '#3B200B'
+        }
       }
     }
   },
