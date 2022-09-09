b120262ccb52128b19a042e6704160a58cf25991
felpfsf <feferreira.sf@gmail.com>
2022-09-09 13:17:14 -0300

    refac: folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Navbar from './components/navbar/Navbar'
 
 const App = () => {
   return (
     <div className='w-[370px] h-[177px] bg-slate-500 p-2'>
       <h1 className='title__main'>Hello React</h1>
       <p className='line-clamp-4 paragraph italic'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos cumque at facilis provident suscipit modi dolor pariatur natus sapiente aperiam, possimus ut minus? In mollitia odio qui repellendus nostrum, excepturi facere! Possimus esse, perferendis laborum, officiis sed pariatur exercitationem necessitatibus quo accusamus fuga et, blanditiis mollitia molestiae illum minus sapiente.</p>
+      <Navbar />
     </div>
   )
 }
